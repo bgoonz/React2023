fbb7fce0f8382e45caebcad7b75565413b50f3a1
Bryan C Guner <bryan.guner@gmail.com>
2023-08-09 13:36:53 -0400

    dynamic class names for steps

--- 2-events-state/src/App.js
+++ 2-events-state/src/App.js
@@ -12,9 +12,9 @@ export default function App() {
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step===1?'active':''}>1</div>
+        <div className={step===2?'active':''}>2</div>
+        <div className={step===3?'active':''}>3</div>
       </div>
       <p className="message">
         Step {step} {messages[step - 1]}
