27659247150e067636d83d384b08274e45f8f00c
Bryan C Guner <bryan.guner@gmail.com>
2023-08-09 14:03:34 -0400

    open and close button for component

--- 2-events-state/src/App.js
+++ 2-events-state/src/App.js
@@ -1,11 +1,8 @@
 import { useState } from "react";
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-];
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 export default function App() {
   const [step, setStep] = useState(1);
+  const [show, setShow] = useState(true);
   const incrementStep = () => {
     setStep(step === 3 ? 1 : step + 1);
   };
@@ -14,29 +11,28 @@ export default function App() {
     setStep(step === 1 ? 3 : step - 1);
   };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step === 1 ? "active" : ""}>1</div>
-        <div className={step === 2 ? "active" : ""}>2</div>
-        <div className={step === 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step} {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={decrementStep}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={incrementStep}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+        <button className="close" onClick={() => setShow(!show)}>&times;</button>
+      {show && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={decrementStep}>
+              Previous
+            </button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={incrementStep}>
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
