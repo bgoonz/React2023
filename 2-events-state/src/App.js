ab8a845492f2a2f850dc544f693af0c78eca806d
Bryan C Guner <bryan.guner@gmail.com>
2023-08-09 13:31:58 -0400

    steps app working

--- 2-events-state/src/App.js
+++ 2-events-state/src/App.js
@@ -1,16 +1,31 @@
+import { useState } from "react";
+const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new income 🤑"];
 export default function App() {
+  const [step, setStep] = useState(1);
+  const incrementStep = () => {
+    setStep(step === 3 ? 1 : step + 1);
+  };
+
+  const decrementStep = () => {
+    setStep(step === 1 ? 3 : step - 1);
+  };
   return (
     <div className="steps">
-      Hello React
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
-      <p className="message">Hello</p>
+      <p className="message">
+        Step {step} {messages[step - 1]}
+      </p>
       <div className="buttons">
-      <button>Previous</button>
-      <button>Next</button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={decrementStep}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={incrementStep}>
+          Next
+        </button>
       </div>
     </div>
   );
