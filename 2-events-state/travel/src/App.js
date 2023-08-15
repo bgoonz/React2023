1e35d0bb9476a1916cbe95cd983bfba4078df2c4
Bryan C Guner <bryan.guner@gmail.com>
2023-08-15 11:48:40 -0400

    components

--- 2-events-state/travel/src/App.js
+++ 2-events-state/travel/src/App.js
@@ -2,4 +2,20 @@ function App() {
   return <div></div>;
 }
 
+function Logo(){
+    return <h1 ></h1>
+}
+
+
+function Form(){
+    
+}
+
+function PackingList(){
+    
+}
+
+function Stats(){
+    
+}
 export default App;
