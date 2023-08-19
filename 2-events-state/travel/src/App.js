37fbd11373457b1fbbcee6910c37de948f128b59
Bryan C Guner <bryan.guner@gmail.com>
2023-08-19 16:22:39 -0400

    reset form after submit

--- 2-events-state/travel/src/App.js
+++ 2-events-state/travel/src/App.js
@@ -4,7 +4,7 @@ const initialItems = [
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Toothbrush", quantity: 1, packed: true },
   { id: 4, description: "Phone charger", quantity: 1, packed: true },
-  { id: 5, description: "Water bottle", quantity: 1, packed: false },
+  { id: 5, description: "Water bottle", quantity: 1, packed: false }
 ];
 function App() {
   return (
@@ -24,24 +24,26 @@ function Form() {
   const [quantity, setQuantity] = useState(1);
   function handleSubmit(event) {
     event.preventDefault();
+    //guard clause against empty description
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    //reset the form
+    setDescription("");
+    setQuantity(1);
+    
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?🧳</h3>
-      <select value={quantity} onChange={(event)=>setQuantity(event.target.value)}>
+      <select value={quantity} onChange={(event) => setQuantity(Number(event.target.value))}>
         {Array.from({ length: 20 }, (_, index) => (
           <option key={index + 1} value={index + 1}>
             {index + 1}
           </option>
         ))}
       </select>
-      <input
-        type="text"
-        name="item"
-        placeholder="Item..."
-        value={description}
-        onChange={(event) => setDescription(event.target.value)}
-      />
+      <input type="text" name="item" placeholder="Item..." value={description} onChange={(event) => setDescription(event.target.value)} />
       <button>Add</button>
     </form>
   );
@@ -60,7 +62,7 @@ function PackingList() {
   return (
     <div className="list">
       {" "}
-      <ul >
+      <ul>
         {initialItems.map((item) => {
           return <Item key={item.id} item={item} />;
         })}
