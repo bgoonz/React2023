5b5df9256b032084a70667fe8d1f1ff10453409c
Bryan C Guner <bryan.guner@gmail.com>
2023-08-19 11:03:17 -0400

    item list

--- 2-events-state/travel/src/App.js
+++ 2-events-state/travel/src/App.js
@@ -1,21 +1,44 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+  ];
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
 }
 
-function Logo(){
-    return <h1 ></h1>
+function Logo() {
+  return <h1>🚢✈️🧳 Far Away🌴🥥🏖️</h1>;
 }
 
-
-function Form(){
-    
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?🧳</h3>
+    </div>
+  );
 }
 
-function PackingList(){
-    
+function PackingList() {
+  return <ul className="list">
+    {initialItems.map((item)=>{
+        return <li key={item.id}>{item.description}</li>
+    })}
+  </ul>;
 }
 
-function Stats(){
-    
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list and you already packed X (X%)</em>
+    </footer>
+  );
 }
 export default App;
