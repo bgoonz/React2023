b334b41dec09f52c5b8d785495bdb412cb0772c4
Bryan C Guner <bryan.guner@gmail.com>
2023-08-19 17:38:10 -0400

    flashcards

--- challenges/flashcards/src/App.js
+++ challenges/flashcards/src/App.js
@@ -1,9 +1,31 @@
-function App() {
+import { useState } from "react";
+// In the file where you want to import the array
+import { questions } from "./questions.js";
+
+export default function App() {
   return (
-    <div>
-      <h1>APP</h1>
+    <div className="App">
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+function FlashCards() {
+  const [selected, setSelected] = useState(0);
+
+  function handleClick(questionId) {
+    setSelected(questionId);
+  }
+
+  return (
+    <div className="flashcards">
+      {questions.map((question) => {
+        return (
+          <div className={selected === question.id ? "selected" : ""} key={question.id} onClick={() => handleClick(question.id)}>
+            <p>{selected !== question.id ? question.question : question.answer}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
