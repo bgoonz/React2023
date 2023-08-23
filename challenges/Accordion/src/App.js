9ae30ad65af272706a228839bb47b3c330d740c7
Bryan C Guner <bryan.guner@gmail.com>
2023-08-23 12:45:40 -0400

    accordion done

--- challenges/Accordion/src/App.js
+++ challenges/Accordion/src/App.js
@@ -1,19 +1,6 @@
 import "./index.css";
-import React from "react";
-const faqs = [
-  {
-    title: "Where are these chairs assembled?",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
-  },
-  {
-    title: "How long do I have to return my chair?",
-    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
-  },
-  {
-    title: "Do you ship to countries outside the EU?",
-    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
-];
+import React, { useState } from "react";
+import { faqs } from "./data";
 
 export default function App() {
   return (
@@ -23,12 +10,16 @@ export default function App() {
   );
 }
 function AccordionItem({ num, title, text }) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => {
+    setShow(!show);
+  };
   return (
-    <div className="item">
-      <p className="number">{num}</p>
+    <div className={show ? "item open" : "item"} onClick={handleClick}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{show ? "-" : "+"}</p>
+      {show ? <div className="content-box">{text}</div> : null}
     </div>
   );
 }
