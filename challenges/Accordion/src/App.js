97038323cd5648ef1d0742714681734d6f7fa993
Bryan C Guner <bryan.guner@gmail.com>
2023-08-23 12:27:07 -0400

    setup for accordion

--- challenges/Accordion/src/App.js
+++ challenges/Accordion/src/App.js
@@ -1,28 +1,43 @@
 import "./index.css";
-
+import React from "react";
 const faqs = [
   {
     title: "Where are these chairs assembled?",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
   },
   {
     title: "How long do I have to return my chair?",
-    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
   },
   {
     title: "Do you ship to countries outside the EU?",
-    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
-  },
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
 ];
 
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
-
-function Accordion() {
-  return <div>TODO</div>;
+function AccordionItem({ num, title, text }) {
+  return (
+    <div className="item">
+      <p className="number">{num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
+}
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((ele, index) => {
+        return <AccordionItem title={ele.title} num={index} text={ele.text} />;
+      })}
+    </div>
+  );
 }
