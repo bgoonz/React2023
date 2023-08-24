88a662e4b93ca98f4befd2f1e377ccaa91654278
Bryan C Guner <bryan.guner@gmail.com>
2023-08-24 12:59:18 -0400

    how react works

--- 4-components/src/components/NavBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NavBar() {
-  return <></>;
-}
-
-export default NavBar;
--- 5-how-react-works/src/index.js
+++ 5-how-react-works/src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 
+import ReactDOM from "react-dom/client";
+// import ReactDOM from 'react-dom';
+
+import App from "./App";
+import "./index.css";
+
+// REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating size={24} color="red" className="test" defaultRating={2} />
-
-    <Test /> */}
   </React.StrictMode>
 );
+
+// REACT 17
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
