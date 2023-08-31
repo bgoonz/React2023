b09877f72becb48b84005f5ed6f1428a02cecbf8
Bryan C Guner <bryan.guner@gmail.com>
2023-08-31 11:35:40 -0400

    starting currency converter app

---
+++ challenges/CurrencyConverter/src/App.js
@@ -0,0 +1,20 @@
+export default function App() {
+  return (
+    <div>
+      <input type="text" />
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
