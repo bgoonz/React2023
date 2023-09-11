0f95f09e94b2825b4f70d77922d9efd692cf54a0
Bryan C Guner <bryan.guner@gmail.com>
2023-09-11 19:26:30 -0400

    useGeolocate

---
+++ challenges/useGeolocate/src/hooks/useGeolocate.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function useGeolocate() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+  return { getPosition, isLoading, countClicks, error, lat, lng };
+}
