3a6c0cdbf003f8ff9101f7de091ead52237f7adf
Bryan C Guner <bryan.guner@gmail.com>
2023-09-12 16:21:21 -0400

    reducer for dataRecieved and dataFailed

--- 8-useReducer/ReactQuiz/src/App.js
+++ 8-useReducer/ReactQuiz/src/App.js
@@ -1,6 +1,41 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active,finished
+  status: "loading"
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error(`Unknown action type: ${action.type}`);
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
