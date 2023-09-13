ee7cc653be3c3bd39034298cd05b324e37ee1af1
Bryan C Guner <bryan.guner@gmail.com>
2023-09-13 13:09:44 -0400

    emoji

--- 8-useReducer/ReactQuiz/src/components/FinishedScreen.js
+++ 8-useReducer/ReactQuiz/src/components/FinishedScreen.js
@@ -1,9 +1,16 @@
 function FinishedScreen({ points, maxPoints, index, numQuestions }) {
   const percentage = Math.ceil((points / maxPoints) * 100);
+  
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
   return (
     <p className="result">
       You scored <strong>{points}</strong> out of <strong>{maxPoints}</strong>{" "}
-      points! ({percentage}%) 🙌
+      points! ({percentage}%) {emoji}
     </p>
   );
 }
