6ad36377fbc85e15821095d01b236dfbcaae1d43
Bryan C Guner <bryan.guner@gmail.com>
2023-09-13 09:19:57 -0400

    option button conditional styling

--- 8-useReducer/ReactQuiz/src/components/Options.js
+++ 8-useReducer/ReactQuiz/src/components/Options.js
@@ -4,8 +4,8 @@ function Options({ question, dispatch, answer }) {
       {question.options.map((option, index) => (
         <button
           key={index}
-          className={`btn btn-option`}
-          onClick={() => dispatch({type: "newAnswer", payload: index})}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${index === question.correctOption ? "correct" : "wrong"}`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
         </button>
