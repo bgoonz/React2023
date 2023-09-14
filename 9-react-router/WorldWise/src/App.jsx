939ca43ee8f7bce9e7826f2075617452d7bdfe0c
Bryan C Guner <bryan.guner@gmail.com>
2023-09-14 12:56:32 -0400

    project synced to starter

--- 9-react-router/WorldWise/src/App.jsx
+++ 9-react-router/WorldWise/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
-import Home from "./pages/Home";
+import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
-import NotFound from "./pages/NotFound";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
 
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- 9-react-router/WorldWise/src/pages/Pricing.jsx
+++ 9-react-router/WorldWise/src/pages/Pricing.jsx
@@ -1,11 +1,24 @@
-import Nav from "../components/Nav";
-function Pricing() {
+// Uses the same styles as Product
+import styles from "./Product.module.css";
+
+export default function Product() {
   return (
-    <div>
-      <h1>Pricing</h1>
-      <Nav />
-    </div>
+    <main className={styles.product}>
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+      </section>
+    </main>
   );
 }
-
-export default Pricing;
