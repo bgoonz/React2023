15c29009d35608ab45609ddbb08c76333d4011f6
Bryan C Guner <bryan.guner@gmail.com>
2023-09-15 11:24:58 -0400

    message component fallback incase there are no cities

--- 9-react-router/WorldWise/src/App.jsx
+++ 9-react-router/WorldWise/src/App.jsx
@@ -6,8 +6,13 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import useFetch from "./hooks/useFetch";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, isLoading] = useFetch(`${BASE_URL}/cities`);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,7 +22,10 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList />} />
-          <Route path="cities" element={<CityList />} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>sibling route to cities</p>} />
           <Route path="form" element={<p>another sibling route</p>} />
         </Route>
--- 9-react-router/WorldWise/src/components/CityList.jsx
+++ 9-react-router/WorldWise/src/components/CityList.jsx
@@ -1,7 +1,19 @@
-import styles from "./CityList.module.css";
+/* eslint-disable react/prop-types */
 
-function CityList() {
-  return <ul className={styles.cityList}>City List</ul>;
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+    if (!cities.length) return <Message message="Add your first city by clicking on a city on the map"/>;
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
