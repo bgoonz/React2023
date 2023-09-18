dbb393f209f57f71027a5e4382c3c91d86b8921f
Bryan C Guner <bryan.guner@gmail.com>
2023-09-18 07:12:22 -0400

    fixed countryList styling

--- 10-context-api/WorldWise/src/components/CountryList.jsx
+++ 10-context-api/WorldWise/src/components/CountryList.jsx
@@ -21,7 +21,7 @@ function CountryList() {
   }, []);
 
   return (
-    <ul className={styles.countriesList}>
+    <ul className={styles.countryList}>
       {countries.map((country) => (
         <CountryItem country={country} key={country.country} />
       ))}
