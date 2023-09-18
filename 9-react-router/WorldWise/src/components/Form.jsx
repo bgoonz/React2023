948769c399bcfd7a2805e4b2c29eda2d66223b27
Bryan C Guner <bryan.guner@gmail.com>
2023-09-17 20:05:38 -0400

    programatic navigation with useNavigate... for Back Button

--- 9-react-router/WorldWise/src/components/Form.jsx
+++ 9-react-router/WorldWise/src/components/Form.jsx
@@ -1,7 +1,8 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
 import styles from "./Form.module.css";
 
 export function convertToEmoji(countryCode) {
@@ -17,40 +18,37 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const navigate = useNavigate();
 
   return (
     <form className={styles.form}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
-        <input
-          id="cityName"
-          onChange={(e) => setCityName(e.target.value)}
-          value={cityName}
-        />
+        <input id="cityName" onChange={(e) => setCityName(e.target.value)} value={cityName} />
         {/* <span className={styles.flag}>{emoji}</span> */}
       </div>
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        />
+        <input id="date" onChange={(e) => setDate(e.target.value)} value={date} />
       </div>
 
       <div className={styles.row}>
         <label htmlFor="notes">Notes about your trip to {cityName}</label>
-        <textarea
-          id="notes"
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
-        />
+        <textarea id="notes" onChange={(e) => setNotes(e.target.value)} value={notes} />
       </div>
 
       <div className={styles.buttons}>
-        <button>Add</button>
-        <button>&larr; Back</button>
+        <Button type="primary">Add</Button>
+        <Button
+          type="back"
+          onClick={(event) => {
+            event.preventDefault();
+            navigate(-1);
+          }}
+        >
+          &larr; Back
+        </Button>
       </div>
     </form>
   );
--- README.md
+++ README.md
@@ -2130,7 +2130,32 @@ export default Map;
 
 ```
 
+**Programatic Navigation with useNavigate**
 
+
+```js
+import styles from "./Map.module.css";
+import { useSearchParams, useNavigate } from "react-router-dom";
+
+function Map() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate('form')}>
+      <h1>Map</h1>
+      <h1>Position: {lat}, {lng}</h1>
+      <button onClick={() => setSearchParams({ lat: 24, lng: 50 })}>
+        Change Position
+      </button>
+    </div>
+  );
+}
+
+export default Map;
+```
 
 
 
