30403aaf7432577099a7286de8cefa12a87ed2fc
Bryan C Guner <bryan.guner@gmail.com>
2023-09-17 20:11:19 -0400

    declaritive navigation with Navigation component

--- 9-react-router/WorldWise/src/App.jsx
+++ 9-react-router/WorldWise/src/App.jsx
@@ -1,5 +1,6 @@
 //Packages
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
 //Components
 import City from "./components/City";
 import CityList from "./components/CityList";
@@ -29,7 +30,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList />} />
+          <Route index element={<Navigate replace to='cities'/>} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
