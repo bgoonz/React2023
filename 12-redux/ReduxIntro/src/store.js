54306699a2f2d5886a3de060766bea54639db4df
Bryan C Guner <bryan.guner@gmail.com>
2023-09-22 14:25:33 -0400

    setup and using redux dev tools

--- 12-redux/ReduxIntro/src/store.js
+++ 12-redux/ReduxIntro/src/store.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
