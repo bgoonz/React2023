fa1288fd93eb65fda0c685a87a2f623a5d0c3073
Bryan C Guner <bryan.guner@gmail.com>
2023-09-22 17:50:03 -0400

    customer slice using redux toolkit

--- 12-redux/ReduxToolkit/src/features/customers/customerSlice.js
+++ 12-redux/ReduxToolkit/src/features/customers/customerSlice.js
@@ -2,22 +2,28 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   fullName: "",
   nationalId: "",
-  createdAt: "",
+  createdAt: ""
 };
 
 const customerSlice = createSlice({
   name: "customer",
   initialState: initialState,
   reducers: {
-    createCustomer(state, action) {
-      state.fullName = action.payload.fullName;
-      state.nationalId = action.payload.nationalId;
-      state.createdAt = action.payload.createdAt;
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return { payload: { fullName, nationalId, createdAt: new Date().toISOString() } };
+      },
+
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      }
     },
     updateName(state, action) {
       state.fullName = action.payload;
-    },
-  },
+    }
+  }
 });
 
 export const { createCustomer, updateName } = customerSlice.actions;
