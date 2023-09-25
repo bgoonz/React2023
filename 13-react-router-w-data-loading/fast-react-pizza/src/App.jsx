4ed4c4cf13284d49a70015d0190edec1ca2f41b8
Bryan C Guner <bryan.guner@gmail.com>
2023-09-25 15:26:02 -0400

    fetching orders from api

--- 13-react-router-w-data-loading/fast-react-pizza/src/App.jsx
+++ 13-react-router-w-data-loading/fast-react-pizza/src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./features/cart/Cart";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order,{loader as orderLoader} from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 import Home from "./ui/Home";
@@ -33,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
