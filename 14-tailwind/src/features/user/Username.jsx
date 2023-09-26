e8305caa6e0ceb59852bd1d71cb022268e39611d
Bryan C Guner <bryan.guner@gmail.com>
2023-09-26 14:01:26 -0400

    tailwind flexbox

--- 14-tailwind/src/features/user/Username.jsx
+++ 14-tailwind/src/features/user/Username.jsx
@@ -1,6 +1,6 @@
 function Username() {
     return (
-        <div className="text-sm font-semibold hidden">
+        <div className="text-sm font-semibold hidden md:block">
             Bryan Guner
         </div>
     )
