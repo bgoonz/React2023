337128188f3a5b162ae67eb3e88ec48f0e7b8423
Bryan C Guner <bryan.guner@gmail.com>
2023-09-26 16:48:15 -0400

    styling checkbox

--- 14-tailwind/src/features/order/CreateOrder.jsx
+++ 14-tailwind/src/features/order/CreateOrder.jsx
@@ -74,6 +74,7 @@ function CreateOrder() {
 
         <div>
           <input
+          className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
