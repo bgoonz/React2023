470e2c27de63d5c2ca94d2331fdf791f54b57e41
Bryan C Guner <bryan.guner@gmail.com>
2023-09-27 12:43:36 -0400

    styled CreateOrder form

--- 14-tailwind/src/features/order/CreateOrder.jsx
+++ 14-tailwind/src/features/order/CreateOrder.jsx
@@ -1,8 +1,7 @@
-/* eslint-disable react-refresh/only-export-components */
-
-import { Form, redirect, useNavigation, useActionData } from 'react-router-dom';
+// import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
-import Button from './../../ui/Button';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,69 +39,66 @@ function CreateOrder() {
 
   const formErrors = useActionData();
 
+  // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let&rsquo;s go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let&rsquo;s go!
+      </h2>
 
-      <Form method="POST" action="/order/new">
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="customer"
-            required
-            placeholder="First Last"
-            className="input"
-          />
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input
-              type="tel"
-              placeholder="111-111-1111"
-              className="input"
-              name="phone"
-              required
-            />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
             <input
+              className="input w-full"
               type="text"
-              placeholder="Your Address"
               name="address"
               required
-              className="input"
             />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
-            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-offset-2"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
-          <Button type="primary" disabled={isSubmitting}>
-            Order now
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <Button disabled={isSubmitting} type="primary">
+            {isSubmitting ? 'Placing order....' : 'Order now'}
           </Button>
         </div>
-        <input type="hidden" name="cart" value={JSON.stringify(cart)} />
       </Form>
     </div>
   );
@@ -111,7 +107,7 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
@@ -119,14 +115,14 @@ export async function action({ request }) {
   };
 
   const errors = {};
-  if (!isValidPhone(order.phone)) {
-    errors.phone = 'Invalid phone number';
-  }
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
 
-  if (Object.keys(errors).length > 0) {
-    return errors;
-  }
-  //If there are no errors ... create the order & redirect.
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
