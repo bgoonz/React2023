5181deec9cc8c9cab1cbd2e10dfe826cf4df6571
Bryan C Guner <bryan.guner@gmail.com>
2023-09-27 12:16:53 -0400

    styled menu and secondary button

--- 14-tailwind/src/features/cart/Cart.jsx
+++ 14-tailwind/src/features/cart/Cart.jsx
@@ -1,26 +1,26 @@
-import { Link } from "react-router-dom";
-import LinkButton from "./../../ui/LinkButton";
-import Button from "./../../ui/Button";
-import CartItem from "./CartItem";
+import { Link } from 'react-router-dom';
+import LinkButton from './../../ui/LinkButton';
+import Button from './../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -36,7 +36,7 @@ function Cart() {
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <ul className="divide-y divide-stone-200  border-b mt-3 ">
+      <ul className="mt-3 divide-y  divide-stone-200 border-b ">
         {cart.map((item) => (
           <CartItem key={item.pizzaId} item={item} />
         ))}
@@ -47,7 +47,6 @@ function Cart() {
           Order pizzas
         </Button>
 
-   
         <Button type="secondary">Clear cart</Button>
       </div>
     </div>
--- 14-tailwind/src/features/cart/CartItem.jsx
+++ 14-tailwind/src/features/cart/CartItem.jsx
@@ -1,5 +1,5 @@
-import Button from "../../ui/Button";
-import { formatCurrency } from "../../utils/helpers";
+import Button from '../../ui/Button';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
