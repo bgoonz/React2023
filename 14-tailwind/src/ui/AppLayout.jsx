13ef3955b3fe4eecb277b4af563e07a4cc039d6b
Bryan C Guner <bryan.guner@gmail.com>
2023-09-26 20:04:42 -0400

    styled loader and removed loader short circuit from AppLayout

--- 14-tailwind/src/ui/AppLayout.jsx
+++ 14-tailwind/src/ui/AppLayout.jsx
@@ -8,8 +8,8 @@ function AppLayout() {
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
-      {/* {isLoading && <Loader />} */}
-      {true && <Loader />}
+      {isLoading && <Loader />}
+    
       <Header />
       <div className=" overflow-scroll ">
         <main className="mx-auto max-w-3xl ">
