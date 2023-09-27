133a448b121dc885629a962349db92043d26b7be
Bryan C Guner <bryan.guner@gmail.com>
2023-09-27 17:18:54 -0400

    remove cart overview if there is no cartQuantity

--- 15-redux-n-advanced-react-router/src/features/cart/CartOverview.jsx
+++ 15-redux-n-advanced-react-router/src/features/cart/CartOverview.jsx
@@ -8,11 +8,13 @@ function CartOverview() {
     const totalCartQuantity = useSelector(getTotalCartQuantity)
     const totalCartPrice = useSelector(getTotalCartPrice)
     console.log(totalCartQuantity)
+    
+    if(!totalCartQuantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 p-4 px-4 py-8 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className=" space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${formatCurrency(totalCartPrice)}</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
--- 15-redux-n-advanced-react-router/src/features/cart/cartSlice.js
+++ 15-redux-n-advanced-react-router/src/features/cart/cartSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  // cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Mediterranean',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
+//   cart: [
+//     {
+//       pizzaId: 12,
+//       name: 'Mediterranean',
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//   ],
 };
 
 const cartSlice = createSlice({
