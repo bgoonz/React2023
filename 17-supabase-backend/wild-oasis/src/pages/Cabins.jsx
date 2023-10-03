fd166efd1beb6cbf7ad59e41d8787c97ab5c5711
Bryan C Guner <bryan.guner@gmail.com>
2023-10-03 17:46:18 -0400

    created storage buckets in supabase

--- 17-supabase-backend/wild-oasis/src/pages/Cabins.jsx
+++ 17-supabase-backend/wild-oasis/src/pages/Cabins.jsx
@@ -1,10 +1,16 @@
+import {useEffect} from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+    // useEffect(() => {
+    //     getCabins().then((cabins) => console.log(cabins));
+    // }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
+      
       <p>TEST</p>
     </Row>
   );
