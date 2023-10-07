cbaf748bfb4b7644e67f630d11a6133b8d2b0c3a
Bryan C Guner <bryan.guner@gmail.com>
2023-10-07 12:21:36 -0400

    done abstracting react query cabins into custom hooks

--- 18-react-query/wild-oasis/src/ui/Sidebar.jsx
+++ 18-react-query/wild-oasis/src/ui/Sidebar.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
 
+
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
@@ -13,6 +14,7 @@ const StyledSidebar = styled.aside`
 `;
 
 function Sidebar() {
+ 
   return (
     <StyledSidebar>
       Sidebar
