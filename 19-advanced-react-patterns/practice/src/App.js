62967a60c5ad1b111f2d562cfc8ae882b9e94e0b
Bryan C Guner <bryan.guner@gmail.com>
2023-10-08 17:02:32 -0400

    fixed css for button more by removing depreciated css

--- 19-advanced-react-patterns/practice/src/App.js
+++ 19-advanced-react-patterns/practice/src/App.js
@@ -96,12 +96,12 @@ export default function App() {
 }
 
 // LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-// function ProductList({ title, items }) {
-//   return (
-//     <ul className="list">
-//       {items.map((product) => (
-//         <ProductItem key={product.productName} product={product} />
-//       ))}
-//     </ul>
-//   );
-// }
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
