c6bc004f989aa3ac645a8398a5dd90ff0ec2a00f
Bryan C Guner <bryan.guner@gmail.com>
2023-10-10 13:41:46 -0400

    api side filtering of bookings is working

--- 20-features/wild-oasis/src/services/apiBookings.js
+++ 20-features/wild-oasis/src/services/apiBookings.js
@@ -1,14 +1,14 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
-    .from("bookings")
-    .select(
-      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)",
-      { count: "exact" },
-    );
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase.from("bookings").select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)");
+  
+  // Filter
+  
+  if(filter !== null) query= query.eq(filter.field, filter.value);
 
+  const { data, error } = await query;
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
@@ -18,11 +18,7 @@ export async function getBookings() {
 }
 
 export async function getBooking(id) {
-  const { data, error } = await supabase
-    .from("bookings")
-    .select("*, cabins(*), guests(*)")
-    .eq("id", id)
-    .single();
+  const { data, error } = await supabase.from("bookings").select("*, cabins(*), guests(*)").eq("id", id).single();
 
   if (error) {
     console.error(error);
@@ -70,9 +66,7 @@ export async function getStaysTodayActivity() {
   const { data, error } = await supabase
     .from("bookings")
     .select("*, guests(fullName, nationality, countryFlag)")
-    .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`,
-    )
+    .or(`and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`)
     .order("created_at");
 
   // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
@@ -87,12 +81,7 @@ export async function getStaysTodayActivity() {
 }
 
 export async function updateBooking(id, obj) {
-  const { data, error } = await supabase
-    .from("bookings")
-    .update(obj)
-    .eq("id", id)
-    .select()
-    .single();
+  const { data, error } = await supabase.from("bookings").update(obj).eq("id", id).select().single();
 
   if (error) {
     console.error(error);
