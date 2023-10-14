95b64482376ab89cd13ea73712d1bd85d3f6076b
Bryan C Guner <bryan.guner@gmail.com>
2023-10-14 19:45:23 -0400

    fixed bug in menus component

--- 20-features/wild-oasis/src/ui/Menus.jsx
+++ 20-features/wild-oasis/src/ui/Menus.jsx
@@ -87,6 +87,8 @@ function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -105,7 +107,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
@@ -113,7 +115,7 @@ function List({ id, children }) {
     <StyledList position={position} ref={ref}>
       {children}
     </StyledList>,
-    document.body,
+    document.body
   );
 }
 
