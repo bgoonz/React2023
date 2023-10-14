587ee364c513834339b8d6d77b65cdae60744a4c
Bryan C Guner <bryan.guner@gmail.com>
2023-10-14 12:46:55 -0400

    using setQueryData to update the cache with local data rather than fetching the data by invalidating the query

--- 20-features/wild-oasis/src/features/authentication/useUpdateUser.js
+++ 20-features/wild-oasis/src/features/authentication/useUpdateUser.js
@@ -5,11 +5,13 @@ export function useUpdateUser() {
   const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({user}) => {
       toast.success("User account sucessfully updated");
-      queryClient.invalidateQueries({
-        queryKey: ["user"]
-      });
+      //the setQueryData method is used to update the cache with local data... the data is also sent to the server via the mutationFn but the cache is updated without needing to fetch the data from the server.
+      queryClient.setQueryData(["user"], user);
+    //   queryClient.invalidateQueries({
+    //     queryKey: ["user"]
+    //   });
     },
     onError: () => {
       toast.error("Cabin could not be created");
