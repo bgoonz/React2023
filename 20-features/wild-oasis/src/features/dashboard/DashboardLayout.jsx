acd552ec72c02b14d2d19db840160cabd6e4448c
Bryan C Guner <bryan.guner@gmail.com>
2023-10-14 19:08:18 -0400

    current day's activity on the dashboard

--- 20-features/wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 20-features/wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import { useCabins } from "../cabins/useCabins";
-import SalesChart from "./SalesChart";
-import Stats from "./Stats";
 import DurationChart from "./DurationChart";
+import SalesChart from "./SalesChart";
+import TodayActivity from "./../check-in-out/TodayActivity";
+import Stats from "./Stats";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 
@@ -28,7 +29,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today&apos;s activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
